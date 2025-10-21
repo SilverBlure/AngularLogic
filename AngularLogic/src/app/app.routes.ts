368295670e1ebf115af7dc742430b90d9a63f23a
Nicolai Österle <oesterle.ni@onlinehome.de>
2025-10-21 18:55:56 +0200

    firs steps

--- AngularLogic/src/app/app.routes.ts
+++ AngularLogic/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FruitListComponent } from './fruit-list/fruit-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: FruitListComponent },
+];
+// 
