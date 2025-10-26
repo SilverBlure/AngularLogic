4b5e7282981c58b9be4093b8db55fd83abe6ec1c
SilverBlure <oesterle.ni@onlinehome.de>
2025-10-26 14:06:41 +0100

    add @INPUT and (click)

--- AngularLogic/src/app/fruit-list/single-fruit/single-fruit.component.ts
+++ AngularLogic/src/app/fruit-list/single-fruit/single-fruit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-single-fruit',
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './single-fruit.component.scss'
 })
 export class SingleFruitComponent {
+  @Input()fruit = {
+      name: "Apfel",
+      img:"apple.png",
+      description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
+      genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
+      stars: 2.3,
+      reviews:[{name: "Kevin W.", text: "ist lecker"},{name: "Arne P.", text: "nicht so meins"}],
+    };
 
+
+    @Output()fruitname = new EventEmitter<string>();
 }
